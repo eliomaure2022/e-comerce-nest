d3e5a5447f72fbcdc7186ee22d380ae347d27b87
eliomaure2022 <elio_maure@yahoo.com.ar>
2024-02-28 17:01:25 -0300

    feat add documentation sawagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +10,24 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
-    }),
+    })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('E-comerce')
+    .setDescription('E-comerce creado en Nest.js')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('user')
+    .addTag('product')
+    .addTag('cart')
+    .addTag('order')
+    .addTag('product-in-cart')
+    .addTag('product-in-order')
+
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
